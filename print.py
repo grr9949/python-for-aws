84cfebc3c98fc82be16e77d5a57d206f22ab0357
grr9949 <grathnakar88@gmail.com>
2024-04-13 00:46:03 +0530

    Added print and variable files

---
+++ print.py
@@ -0,0 +1,6 @@
+#print string
+#print("Hello World!")
+#print integer
+#print(1802)
+#concatinate function 
+print("Hello World!\n" + str(1802))
---
+++ variable.py
@@ -0,0 +1,23 @@
+# Variables: When ever we want to use a value multiple times in a code we will give them as variable
+#            and will call those variables while executing code
+
+
+"""#Creating a variable for a string
+name= "Ishaan"
+profession="Writer"
+#Creating a variable for a integer
+books_written= 6
+#Concatenate variables
+output=name + " is a " + profession + " and he has written " + str(books_written) + " books"
+output2= f"{name} is a {profession} and he has written {books_written} books"
+
+print(output)
+print(output2)"""
+
+genre="Fiction"
+first_book="Moon of Solace"
+award="Best Fiction Writer 2023"
+
+author_bio= f"Ishaan is a {genre} writer, is best known for his first book {first_book} and was awarded The {award}"
+
+print(author_bio)
