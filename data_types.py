c1eddeffc45f18077f9317659c7554d5a60560ee
grr9949 <grathnakar88@gmail.com>
2024-04-13 15:14:46 +0530

    Added data types and operators

---
+++ data_types.py
@@ -0,0 +1,19 @@
+
+def main():
+    #String 
+    instane_type='t2.miro'
+
+    #integer
+    num_of_instances=5
+    up_time=10
+
+    #float point number
+    price=0.25
+
+    #Boolean
+    Instance_State=True
+
+    print(f"I have {num_of_instances} instances of type {instane_type} running for {up_time} hours and per hour price is {price} USD.\nState is running = {Instance_State} ")
+
+if __name__ == '__main__':
+    main()
---
+++ operators.py
@@ -0,0 +1,36 @@
+#Deterimine if the shipping list item is eligible for free shipping & discount
+
+def main():
+    """Assignment operator"""
+    #Set item name, quantity, item price, discount rate and eligible items
+    item_name="Banana"
+    quantity= 5
+    item_price= 2 #USD
+    discount_rate= 0.1
+    eligible_items= "Orange Banana Carrot"
+
+    """Arthimetic Operators"""
+    #Calculate Subtotal
+    Subtotal=quantity*item_price
+    #Print Item name and Subtotal
+    print(f"Item name : {item_name}, Subtotal : {Subtotal}")
+    
+    #Check if item name is eligible for discount
+    if item_name in eligible_items:
+        discount=Subtotal*discount_rate
+    print(f"discount: {discount}")
+
+    #Comparision operators
+    #Check if discount applied (discount>0)
+    was_discount_applied= discount>0
+    print(f"was discount applied : {was_discount_applied}")
+
+    #logical operators
+    #Check if free shipping is eligible or not
+    Does_free_shipping_apply= quantity>=5 and item_name=="Banana"
+    print(f"Is this item eligible for free shipping ? {Does_free_shipping_apply}")
+
+if __name__ == '__main__':
+    main()
+
+
